02eb07b4e776d7d046135db6c310fb7fa7783cd9
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-02-21 10:39:17 +0200

    New sensors library written for compatibility with sensor input events (proximity and light)

--- libsensors/LightSensor.cpp
+++ libsensors/LightSensor.cpp
@@ -28,130 +28,95 @@
 
 #include "LightSensor.h"
 
-
 /*****************************************************************************/
-/* The lightsensor for msm7x27 delivers: 0, 4, 16, 64, 250 depending on the ambiebt light */
-
-#define LIGHT_SENSOR "/sys/devices/platform/i2c-adapter/i2c-0/0-0036/als::value"
-int thread_running_light=0;
 
 LightSensor::LightSensor()
-	: SensorBase(LS_DEVICE_NAME, "lightsensor"),
-	  mInputReader(4),
-	  mHasPendingEvent(false)
+    : SensorBase(LS_DEVICE_NAME, "lightsensor"),
+      mEnabled(0),
+      mInputReader(4),
+      mHasPendingEvent(false)
 {
-	mPendingEvent.version = sizeof(sensors_event_t);
-	mPendingEvent.sensor = ID_L;
-	mPendingEvent.type = SENSOR_TYPE_LIGHT;
-	memset(mPendingEvent.data, 0, sizeof(mPendingEvent.data));
+    mPendingEvent.version = sizeof(sensors_event_t);
+    mPendingEvent.sensor = ID_L;
+    mPendingEvent.type = SENSOR_TYPE_LIGHT;
+    memset(mPendingEvent.data, 0, sizeof(mPendingEvent.data));
 
-	setInitialState();
+     open_device();
+     setInitialState();
 }
 
 LightSensor::~LightSensor() {
 }
 
 int LightSensor::setInitialState() {
-	if (thread_running_light == 0) {
-		int rc = pthread_create(&poll_thread, NULL, polling_thread_func_light, (void *)this);
-		if (rc){
-			LOGE("ERROR return code from pthread_create() is %d\n", rc);
-		}
-		else{
-			//LOGE("LightSensor created new thread");
-		}
-	}
-	return 0;
+    return 0;
 }
-
-void * polling_thread_func_light(void *PS)
-{
-	thread_running_light = 1;
-	char buf [15]="0";
-	char prev_value [15]="0";
-	LightSensor * a = (LightSensor*) PS;
-	FILE * light_fd;
-	if ((light_fd = fopen(LIGHT_SENSOR, "r")) < 0) {
-		LOGE("LightSensor cannot be opened");
-		thread_running_light = 0;
-		if (light_fd)
-			fclose(light_fd);
-		pthread_exit(NULL);
-	}
-	while (1) {
-		light_fd = freopen(LIGHT_SENSOR, "r", light_fd);
-		fgets(buf, 10, light_fd); 
-		//LOGE("LightSensor thread says: %s, prev was: %s", buf, prev_value);
-		// Deliver an event only if the light sensor value has changed
-		if (strcmp(prev_value, buf) !=0) {
-			a->mPendingEvent.light = atoi(buf);
-			a->mHasPendingEvent = true;
-			//LOGE("LightSensor thread delivering: %f", a->mPendingEvent.light);
-			strcpy(prev_value,buf);
-		}
-		sleep(2);
-	}
-	return 0;
-}
-
 
 int LightSensor::enable(int32_t, int en) {
-	if(en) {
-		setInitialState();
-	}
-	return 0;
+	int newState = en ? 1 : 0;
+	if (newState != mEnabled) {
+		this->mPendingEvent.light = -1;
+		this->mHasPendingEvent = true;
+		mEnabled = newState;
+		return 0;
+    }
+    return 0;
 }
 
 bool LightSensor::hasPendingEvents() const {
-	return mHasPendingEvent;
+    return mHasPendingEvent;
 }
 
 int LightSensor::readEvents(sensors_event_t* data, int count)
 {
-	if (count < 1)
-		return -EINVAL;
+    if (count < 1)
+        return -EINVAL;
 
-	if (mHasPendingEvent) {
-		mHasPendingEvent = false;
-		mPendingEvent.timestamp = getTimestamp();
-		*data = mPendingEvent;
-		return 1;
-	}
+    if (mHasPendingEvent) {
+        mHasPendingEvent = false;
+        mPendingEvent.timestamp = getTimestamp();
+        *data = mPendingEvent;
+        return mEnabled ? 1 : 0;
+    }
+    ssize_t n = mInputReader.fill(data_fd);
+    if (n < 0)
+        return n;
+    int numEventReceived = 0;
+    input_event const* event;
 
-	ssize_t n = mInputReader.fill(data_fd);
-	if (n < 0)
-		return n;
+    while (count && mInputReader.readEvent(&event)) {
+        int type = event->type;
+        if (type == EV_ABS) {
+        	if (event->value != -1) {
+            	mPendingEvent.light = indexToValue(event->value);
+            }
+        } else if (type == EV_SYN) {
+            mPendingEvent.timestamp = timevalToNano(event->time);
+            if (mEnabled) {
+                *data++ = mPendingEvent;
+                count--;
+                numEventReceived++;
+            }
+        } else {
+            LOGE("LightSensor: unknown event (type=%d, code=%d)",
+                    type, event->code);
+        }
+        mInputReader.next();
+    }
 
-	int numEventReceived = 0;
-	input_event const* event;
-
-	while (count && mInputReader.readEvent(&event)) {
-		int type = event->type;
-		if (type == EV_LED) { // light sensor 1
-			if (event->code == EVENT_TYPE_LIGHT) {
-				mPendingEvent.light = event->value;
-			}
-		} else if (type == EV_MSC) { // light sensor 2
-			/* Light sensor 2 seems to put out lower lux values than
-			 * light sensor 1, and I can't decide which is more "accurate",
-			 * so for now just disabling #2
-
-			if (event->code == EVENT_TYPE_LIGHT2) {
-				mPendingEvent.light = event->value;
-			}
-
-			 */
-		} else if (type == EV_SYN) {
-			mPendingEvent.timestamp = timevalToNano(event->time);
-			*data++ = mPendingEvent;
-			count--;
-			numEventReceived++;
-		} else {
-			LOGE("LightSensor: unknown event (type=%d, code=%d)",
-					type, event->code);
-		}
-		mInputReader.next();
-	}
-
-	return numEventReceived;
+    return numEventReceived;
 }
+
+/* The lightsensor for msm7x27 delivers 5 levels of light: 0, 4, 16, 64, 250 depending on the ambient light */
+float LightSensor::indexToValue(size_t index) const
+{
+    static const float luxValues[5] = {
+    		0, 4, 16, 64, 250
+    };
+
+    const size_t maxIndex = sizeof(luxValues)/sizeof(*luxValues) - 1;
+    if (index > maxIndex)
+        index = maxIndex;
+    return luxValues[index];
+}
+
